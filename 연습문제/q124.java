c911f227c6c54bd4dc5ea592137ad778d421701d
JYL-Backend <dustmqdyd1213@gmail.com>
2022-11-12 13:47:05 +0900

    코딩테스트 연습 (124나라)

---
+++ 연습문제/q124.java
@@ -0,0 +1,18 @@
+package 연습문제;
+
+public class q124 {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        while(n > 0){
+            if(n%3 == 0){
+                sb.insert(0, 4);
+                n = n/3-1;
+            }
+            else {
+                sb.insert(0, String.valueOf(n%3));
+                n = n/3;
+            }
+        }
+        return sb.toString();
+    }
+}
